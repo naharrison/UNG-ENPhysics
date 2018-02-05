0245fa09ea5165c5be78ef51d893a40444ff9e68
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2018-02-05 16:40:07 -0500

    Cleaned up trainNetwork.py

--- src/main/python/edu/ung/phys/trainNetwork.py
+++ src/main/python/edu/ung/phys/trainNetwork.py
@@ -1,45 +1,79 @@
-
-from sklearn.neural_network import MLPClassifier
+import sys
 import numpy
-g = 0
-X = [[0.,0.,0.,0.,0.]] # inputs as an array of lists
-y = [0.] # target values
+from sklearn.neural_network import MLPClassifier
 
-clf = MLPClassifier(solver='adam', alpha=1e-5, random_state=1)
+"""
+Read in the training and test files from the command line
+or use the default values
+"""
+trainingFileName = "test-data/pid_sim_200.txt"
+testFileName = "test-data/pid_sim_10.txt"
+
+if(len(sys.argv) != 1 and len(sys.argv) != 3):
+    print("wrong number of args... exiting")
+    sys.exit()
+elif(len(sys.argv) == 3): 
+    trainingFileName = sys.argv[1] # (sys.argv[0] is the script filename)
+    testFileName = sys.argv[2]
 
 
-training_data_file = open("test-data/pid_sim_200.txt", 'r')
+"""
+Read training data into a big matrix (an array of arrays).
+Ok for relatively small files but may need a smarter system for large files.
+Also read target values into an array.
+"""
+training_data_matrix = []
+training_target_values = [] # i.e. true values
+
+training_data_file = open(trainingFileName, 'r')
 training_data_list = training_data_file.readlines()
 training_data_file.close()
 
 for record in training_data_list:
-	all_values = record.split(',')
-	#X.append((numpy.asfarray(all_values[1:])))
-	#print(X)
-	X.append(numpy.array(list(map(float, all_values[1:]))).tolist())
-	y.append(float(all_values[0]))
+    all_values = record.split(',')
+    training_data_matrix.append(list(map(float, all_values[1:])))
+    training_target_values.append(float(all_values[0])) # will int work here??
 
-print(clf.fit(X,y))
 
-X = [[0.,0.,0.,0.,0.]]
-y = []
-training_data_file = open("test-data/pid_sim_10.txt", 'r')
-training_data_list = training_data_file.readlines()
-training_data_file.close()
+"""
+Create and train the classifier.
+MLP = Multi-Layer Perceptron.
+"""
+classifier = MLPClassifier(solver='adam', alpha=1e-5, random_state=1)
+classifier_parameters = classifier.fit(training_data_matrix, training_target_values)
+print("\nThe classifier parameters are as follows:\n")
+print(classifier_parameters, "\n")
 
-for record in training_data_list:
-	all_values = record.split(',')
-	X.append(numpy.array(list(map(float, all_values[1:]))).tolist())
-	y.append(float(all_values[0]))
 
-for g in training_data_list:
-        prediction = ','.join(map(str ,clf.predict(X)))
-##        predictions = float(prediction.split(","))
-        predictions = [int(e) if e.isdigit() else e for e in prediction.split(',')]
+"""
+Read in some test data.
+"""
+test_data_matrix = []
+test_target_values = [] # i.e. true values
 
-#print(clf.score(X,y))
-#prediction = ','.join(map(str ,clf.predict(X)))
-#print(prediction)
+test_data_file = open(testFileName, 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
 
-print(clf.predict(X))
-print(y)
+for record in test_data_list:
+    all_values = record.split(',')
+    test_data_matrix.append(list(map(float, all_values[1:])))
+    test_target_values.append(float(all_values[0])) # will int work here??
+
+
+"""
+Now test the classifier on the test data and print results
+"""
+predictions = classifier.predict(test_data_matrix)
+# score = classifier.score(test_data_matrix, test_target_values)
+
+number_correct = 0
+
+for k in range(0, len(predictions)):
+    if(predictions[k] == test_target_values[k]):
+        print("Test event {}: actual={}, prediction={}  :)".format(k+1, test_target_values[k], predictions[k]))
+        number_correct += 1
+    else:
+        print("Test event {}: actual={}, prediction={}  :(".format(k+1, test_target_values[k], predictions[k]))
+
+print("\n{} out of {} correct".format(number_correct, len(predictions)))
