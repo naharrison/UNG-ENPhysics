ffdfb41b28424daedc6e39883d77da41b6b7963e
CJBrow1590 <35770002+CJBrow1590@users.noreply.github.com>
2018-02-13 16:25:21 -0500

    Updated trainNetwork to print info related to its incorrect guesses

--- Neural Network/trainNetwork.py
+++ Neural Network/trainNetwork.py
@@ -1,46 +1,93 @@
-
-from sklearn.neural_network import MLPClassifier
+import sys
 import numpy
-g = 0
-X = [[0.,0.,0.,0.,0.]] # inputs as an array of lists
-y = [0.] # target values
+from sklearn.neural_network import MLPClassifier
 
-clf = MLPClassifier(solver='adam', alpha=1e-5, random_state=1)
+"""
+Read in the training and test files from the command line
+or use the default values
+"""
+trainingFileName = "data/data_train_5000.txt"
+testFileName = "data/data_test_5000.txt"
+
+if(len(sys.argv) != 1 and len(sys.argv) != 3):
+    print("wrong number of args... exiting")
+    sys.exit()
+elif(len(sys.argv) == 3): 
+    trainingFileName = sys.argv[1] # (sys.argv[0] is the script filename)
+    testFileName = sys.argv[2]
 
 
-training_data_file = open("data/data_train_5000.txt", 'r')
+"""
+Read training data into a big matrix (an array of arrays).
+Ok for relatively small files but may need a smarter system for large files.
+Also read target values into an array.
+"""
+training_data_matrix = []
+training_target_values = [] # i.e. true values
+
+training_data_file = open(trainingFileName, 'r')
 training_data_list = training_data_file.readlines()
 training_data_file.close()
 
 for record in training_data_list:
-	all_values = record.split(',')
-	#X.append((numpy.asfarray(all_values[1:])))
-	#print(X)
-	X.append(numpy.array(list(map(float, all_values[1:]))).tolist())
-	y.append(float(all_values[0]))
+    all_values = record.split(',')
+    training_data_matrix.append(list(map(float, all_values[1:])))
+    training_target_values.append(float(all_values[0])) # will int work here??
 
-print(clf.fit(X,y))
 
-X = [[0.,0.,0.,0.,0.]]
-y = []
-training_data_file = open("data/data_test_10.txt", 'r')
-training_data_list = training_data_file.readlines()
-training_data_file.close()
+"""
+Create and train the classifier.
+MLP = Multi-Layer Perceptron.
+"""
+classifier = MLPClassifier(solver='adam', activation='relu', alpha=1e-5)
+classifier_parameters = classifier.fit(training_data_matrix, training_target_values)
+##print("\nThe classifier parameters are as follows:\n")
+##print(classifier_parameters, "\n")
 
-for record in training_data_list:
-	all_values = record.split(',')
-	X.append(numpy.array(list(map(float, all_values[1:]))).tolist())
-	y.append(float(all_values[0]))
+"""
+Read in some test data.
+"""
+test_data_matrix = []
+test_target_values = [] # i.e. true values
 
-for g in training_data_list:
-        prediction = ','.join(map(str ,clf.predict(X)))
-##        predictions = float(prediction.split(","))
-        predictions = [int(e) if e.isdigit() else e for e in prediction.split(',')]
-        print(prediction[g])
+test_data_file = open(testFileName, 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
 
-#print(clf.score(X,y))
-prediction = ','.join(map(str ,clf.predict(X)))
-print(prediction)
+for record in test_data_list:
+    all_values = record.split(',')
+    test_data_matrix.append(list(map(float, all_values[1:])))
+    test_target_values.append(float(all_values[0])) # will int work here??
 
-##print(clf.predict(X))
-#print(y)
+
+"""
+Now test the classifier on the test data and print results
+
+score outputs a percent and it is multiplied by the number or events (len of predictions) to get the score in 'correct out of total' format
+"""
+predictions = classifier.predict(test_data_matrix)
+score = classifier.score(test_data_matrix, test_target_values)
+print (int(score*len(predictions)), "out of {} correct".format(len(predictions)))
+
+number_correct = 0
+
+
+wrong_guess = []
+less_4 = 0
+great_4 = 0
+
+for k in range(0, len(predictions)):
+
+    if(predictions[k] == test_target_values[k]):
+        number_correct += 1
+    else:
+        wrong_guess += [test_data_matrix[k][0]]
+for t in range(0, len(wrong_guess)):
+    if(wrong_guess[t] <= 0.4):
+        less_4 += 1
+    else:
+        great_4 += 1
+##print(len(wrong_guess))
+print("Wrong guesses with data point 1 less than 0.4", less_4)
+print("Wrong guesses with data point 1 greater than 0.4", great_4)
+
