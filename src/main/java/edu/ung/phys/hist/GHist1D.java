73ce71fc4c772b9af2deaafb4cbee28f670ba681
Nathan Harrison <nathan.andrew.harrison@gmail.com>
2018-02-16 19:04:13 -0500

    Added a 1D hist

---
+++ src/main/java/edu/ung/phys/hist/GHist1D.java
@@ -0,0 +1,43 @@
+package edu.ung.phys.hist;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import processing.core.PApplet;
+
+public class GHist1D {
+
+	public PApplet parent;
+	public GHorizontalAxis gaxisX;
+	public ArrayList<Integer> counts;
+
+	private GVerticalAxis gaxisY;
+	private int max;
+	
+
+	public GHist1D(PApplet parent, GHorizontalAxis gaxisX, ArrayList<Integer> counts) {
+		this.parent = parent;
+		this.gaxisX = gaxisX;
+		this.counts = counts;
+		this.max = Collections.max(counts);
+		
+		gaxisY = new GVerticalAxis(parent, new Axis(2, 0, max));
+	}
+
+	
+	public void display() {
+		gaxisX.display();
+		gaxisY.display();
+
+		parent.stroke(50);
+		parent.strokeWeight((float) 4.00);
+
+		int boxWidth = (int) (((double) parent.width)/((double) counts.size()));
+		int boxHeight = 8;
+		for(int j = 0; j < counts.size(); j++) {
+			int ycoord = (int) (parent.height - (((double) counts.get(j))/((double) max))*parent.height);
+			parent.rect(j*boxWidth, ycoord - boxHeight/2, boxWidth, boxHeight);
+		}
+	}
+
+}
--- src/main/java/edu/ung/phys/hist/PlotTry.java
+++ src/main/java/edu/ung/phys/hist/PlotTry.java
@@ -1,5 +1,8 @@
 package edu.ung.phys.hist;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import processing.core.PApplet;
 
 public class PlotTry extends PApplet {
@@ -13,6 +16,7 @@ public class PlotTry extends PApplet {
 	GHorizontalAxis xAxis;
 	GVerticalAxis yAxis;
 	GHist2D ghist2d;
+	GHist1D ghist1d;
 
 
 	public void settings() {
@@ -35,20 +39,31 @@ public class PlotTry extends PApplet {
 		gridValues.set(35, 1, 3);
 		gridValues.set(3, 2, 3);
 		
+		ArrayList<Integer> h1dValues = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 2, 1, 0, 5));
+		
 		grid = new ColorGrid2D(this, gridValues);
 		
 		xAxis = new GHorizontalAxis(this, new Axis(4, 10, 20));
 		yAxis = new GVerticalAxis(this, new Axis(5, 40, 90));
 		
 		ghist2d = new GHist2D(this, grid, xAxis, yAxis);
+
+		ghist1d = new GHist1D(this, xAxis, h1dValues);
 	}
 	
 	
 	public void draw() {
 		background(220);
+
 		pushMatrix();
-		translate(100, 50);
-		scale((float) 0.8, (float) 0.8);
+		translate(50, 25);
+		scale((float) 0.4, (float) 0.4);
+		ghist1d.display();
+		popMatrix();
+
+		pushMatrix();
+		translate(440, 320);
+		scale((float) 0.4, (float) 0.4);
 		ghist2d.display();
 		popMatrix();
 	}
