eac99cf52538ffeb5d3a49b1d703c57a9bfbaf60
Cody Laptop <cjbrow1590@ung.edu>
2018-09-22 11:25:54 -0400

    Fixed bugs in Pur Eff Tracker

--- src/main/java/edu/ung/phys/EfficiencyPurityTracker.java
+++ src/main/java/edu/ung/phys/EfficiencyPurityTracker.java
@@ -1,9 +1,12 @@
 ///program on efficiency and purity of data//
+package edu.ung.phys;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap
+import java.util.HashMap;
+
 
 public class EfficiencyPurityTracker {
 	
@@ -13,35 +16,36 @@ public class EfficiencyPurityTracker {
 	public EfficiencyPurityTracker(int nSpecies, ArrayList<Integer> ids) {
 	   this.nSpecies = nSpecies;
 	   this.ids = ids;
-	   totalOccurances = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
-	   nLowCon_Corr = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
-	   nLowCon_incorr = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
-	   nHighCon_corr = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
-	   nHighCon_incorr = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
-	   nUnknown = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
-	   nTies = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
+	   this.totalOccurances = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
+	   this.nLowCon_corr = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
+	   this.nLowCon_incorr = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
+	   this.nHighCon_corr = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
+	   this.nHighCon_incorr = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
+	   this.nUnknown = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
+	   this.nTies = new ArrayList<Integer>(Collections.nCopies(nSpecies, 0));
 	}
 
-	public void track(int actual, int prediction, ConfidenceLevel confidence)
+	public void track(int actual, int prediction, ConfidenceLevel confidence){
 	  if (confidence == ConfidenceLevel.UNKNOWN) {
 	  	nUnknown.set(ids.get(actual), nUnknown.get(ids.get(actual)) + 1);
 	  }
-	  if else(confidence == ConfidenceLevel.TIE) {
+	  else if(confidence == ConfidenceLevel.TIE) {
 		nTies.set(ids.get(actual), nTies.get(ids.get(actual)) + 1);
 	  }
-	  if else(prediction == actual && confidence == ConfidenceLevel.HIGH) {
+	  else if(prediction == actual && confidence == ConfidenceLevel.HIGH) {
  		nHighCon_corr.set(ids.get(actual), nHighCon_corr.get(ids.get(actual)) + 1);
 	  }
-	  if else(prediction == actual && confidence == ConfidenceLevel.LOW) {
+	  else if(prediction == actual && confidence == ConfidenceLevel.LOW) {
 		nLowCon_corr.set(ids.get(actual), nLowCon_corr.get(ids.get(actual)) + 1);
 	  }
-	  if else(prediction != actual && confidence == ConfidenceLevel.HIGH) {
+	  else if(prediction != actual && confidence == ConfidenceLevel.HIGH) {
 		nHighCon_incorr.set(ids.get(actual), nHighCon_incorr.get(ids.get(actual)) + 1);
 	  }
-	  if else(prediction != acutal && confidence == ConfidenceLevel.LOW) {
+	  else if(prediction != actual && confidence == ConfidenceLevel.LOW) {
 		nLowCon_incorr.set(ids.get(actual), nLowCon_incorr.get(ids.get(actual)) + 1);
 	  }
 	  else {
-		System.out.println("Something is very wrong")
+		System.out.println("Something is very wrong");
 	  }
+	}
  }
