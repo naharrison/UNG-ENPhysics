9d3b34b21673e16823a0265edc82609675ad29f8
mflor9255 <36076994+mflor9255@users.noreply.github.com>
2018-10-07 15:01:14 -0400

    cut based pid updates (#15)
    * starting positive particlee identifier code
    
    * Progress on cut based PID
    
    * Finished adding cuts to CutBasedPid

--- src/main/java/edu/ung/phys/CutBasedPid.java
+++ src/main/java/edu/ung/phys/CutBasedPid.java
@@ -26,14 +26,21 @@ public class CutBasedPid {
     bMax = 1.15;
 
     posH = new F1D("posH", "1.075 + 0.0*x", pMin, 0.75);
-    posH.setLineWidth(4);
-    posL = new F1D("posL", "0.91 + 0.25*x", pMin, 0.75);
-    posL.setLineWidth(4);
-    piH  = new F1D("piH", "1.05 + 0.0*x", pMin, pMax);
-    piH.setLineWidth(4);
-    piL  = new F1D("piL", "0.5*(sqrt(x*x/(0.14*0.14 + x*x)) + sqrt(x*x/(0.493*0.493 + x*x)))", pMin, pMax);
-    piL.setLineWidth(4);
-    // etc...
+    posH.setLineWidth(3);
+    posL = new F1D("posL", "0.91 + 0.333*x", pMin, 0.75);
+    posL.setLineWidth(3);
+    piH  = new F1D("piH", "1.05 + 0.0*x", pMin + 0.1, pMax);
+    piH.setLineWidth(3);
+    piL  = new F1D("piL", "0.48*(sqrt(x*x/(0.14*0.14 + x*x)) + 0.98sqrt(x*x/(0.493*0.493 + x*x)))", pMin + 0.1, pMax);
+    piL.setLineWidth(3);
+    kH = new F1D("kH", "0.6666*(sqrt(x*x/(0.494*0.494 + x*x))) +  0.3333*(sqrt(x*x/(0.14*0.14 + x*x)))", pMin + 0.1, pMax);
+    kH.setLineWidth(3);
+    kL = new F1D("kL", "0.6666*(sqrt(x*x/(0.494*0.494 + x*x))) + 0.3333*(sqrt(x*x/(0.938*0.938 + x*x)))", pMin + 0.1, pMax);
+    kL.setLineWidth(3);
+    prH = new F1D("prH", "0.35*(sqrt(x*x/(0.938*0.938 + x*x))) + 0.7*(sqrt(x*x/(0.494*0.494 + x*x)))", pMin + 0.1, pMax);
+    prH.setLineWidth(3);
+    prL = new F1D("prL", "1.5*(sqrt(x*x/(0.938*0.938 + x*x))) - 0.5*(sqrt(x*x/(0.494*0.494 + x*x)))", pMin + 0.1, pMax);
+    prL.setLineWidth(3);
 
     ArrayList<Integer> ids = new ArrayList<>();
     ids.add(-11);
@@ -53,7 +60,7 @@ public class CutBasedPid {
 
   public static void runAnalysis() throws IOException {
     int n = 649951;
-    PidTestDataReader reader = new PidTestDataReader("/Users/naharrison/master/data-samples/e1f/Pid-Data/pidout-649951.txt", n);
+    PidTestDataReader reader = new PidTestDataReader("/home/mflor9255/data-samples/e1f/Pid-Data/pidout-649951.txt", n);
     for(int k = 0; k < n; k++) {
       String[] values = reader.getNextEvent();
       int trueID = Integer.parseInt(values[0]);
@@ -66,12 +73,18 @@ public class CutBasedPid {
       else if(trueID == 2212) bvp_pr.fill(p, beta);
       else System.out.println("CRAP!!!");
 
+      ConfidenceLevel confidence = ConfidenceLevel.HIGH;
       int predictionID;
       if(beta < posH.evaluate(p) && beta > posL.evaluate(p)) predictionID = -11;
-      else predictionID = 2212;
-      // finish this...
+      else if(beta < piH.evaluate(p) && beta > piL.evaluate(p)) predictionID = 211;
+      else if(beta < kH.evaluate(p) && beta > kL.evaluate(p)) predictionID = 321;
+      else if(beta < prH.evaluate(p) && beta > prL.evaluate(p)) predictionID = 2212;
+      else {
+        predictionID = 2212;
+        confidence = ConfidenceLevel.UNKNOWN;
+      }
 
-      epTracker.trackActualPredicted(trueID, predictionID, ConfidenceLevel.HIGH);
+      epTracker.trackActualPredicted(trueID, predictionID, confidence);
     }
   }
 
@@ -88,21 +101,37 @@ public class CutBasedPid {
     can.draw(posL, "same");
     can.draw(piH, "same");
     can.draw(piL, "same");
+    can.draw(kH, "same");
+    can.draw(kL, "same");
+    can.draw(prH, "same");
+    can.draw(prL, "same");
 
     TCanvas can2 = new TCanvas("can2", 1100, 450);
     can2.divide(4, 1);
+
     can2.cd(0);
     can2.getPad().getAxisZ().setLog(true);
     can2.draw(bvp_pos);
+    can2.draw(posH, "same");
+    can2.draw(posL, "same");
+
     can2.cd(1);
     can2.getPad().getAxisZ().setLog(true);
     can2.draw(bvp_pi);
+    can2.draw(piH, "same");
+    can2.draw(piL, "same");
+
     can2.cd(2);
     can2.getPad().getAxisZ().setLog(true);
     can2.draw(bvp_k);
+    can2.draw(kH, "same");
+    can2.draw(kL, "same");
+
     can2.cd(3);
     can2.getPad().getAxisZ().setLog(true);
     can2.draw(bvp_pr);
+    can2.draw(prH, "same");
+    can2.draw(prL, "same");
 
     System.out.println("");
     for(int pid : epTracker.ids) System.out.println(pid + " " + epTracker.getEfficiency(pid) + " " + epTracker.getPurity(pid));
